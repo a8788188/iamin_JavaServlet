a7f6dc64b7dcc0f51cee073164006d1f910274fc
林以樂 <a0989758574@gmail.com>
2021-07-28 22:30:40 +0800

    ReportDao 新增方法 取得delet_time不是空的所有被檢舉人id. 並拿到該id的member物件 2021/07/28

--- src/main/java/com/dao/ReportDao.java
+++ src/main/java/com/dao/ReportDao.java
@@ -1,9 +1,13 @@
 package com.dao;
 
+import java.util.List;
 import com.bean.Report;
 
 public interface ReportDao {
 	
 	int insert(Report report);
+	
+	//取得有被檢舉的memberId(delete_time 不是空)
+	List<Integer> selectmemberidreport();
 
 }
--- src/main/java/com/dao/implemen/ReportDaoImp.java
+++ src/main/java/com/dao/implemen/ReportDaoImp.java
@@ -2,7 +2,10 @@ package com.dao.implemen;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.sql.DataSource;
 
@@ -37,6 +40,30 @@ public class ReportDaoImp implements ReportDao {
             e.printStackTrace();
             return -1;
         }
+	}
+
+	@Override
+	public List<Integer> selectmemberidreport() {
+		List<Integer> reported_member_id = new ArrayList<Integer>();
+		String sql = "SELECT REPORTED_ID " + 
+					 "from report " + 
+					 "where DELETE_TIME IS NULL " + 
+					 "group by REPORTED_ID; ";
+		try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql);
+        ) {
+			 ResultSet rs = ps.executeQuery();
+			 
+			 while (rs.next()) {
+				reported_member_id.add(rs.getInt(1));
+			}
+			 return reported_member_id;
+		}catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
 	}
 
 }
